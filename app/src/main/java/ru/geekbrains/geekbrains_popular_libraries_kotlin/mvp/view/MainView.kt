4e35a2a4a229a841f331a4ed7490d1cd05fcf0ca
es.starkov <>
2021-03-22 21:20:17 +0400

    new screen user fragment, displayed user login

--- app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/view/MainView.kt
+++ app/src/main/java/ru/geekbrains/geekbrains_popular_libraries_kotlin/mvp/view/MainView.kt
@@ -1,7 +1,15 @@
 package ru.geekbrains.geekbrains_popular_libraries_kotlin.mvp.view
 
-interface MainView {
-    fun setButtonTextOne(text: String)
-    fun setButtonTextTwo(text: String)
-    fun setButtonTextThree(text: String)
-}
+import moxy.MvpView
+import moxy.viewstate.strategy.alias.AddToEndSingle
+
+@AddToEndSingle
+interface MainView : MvpView
+
+
+
+//interface MainView {
+//    fun setButtonTextOne(text: String)
+//    fun setButtonTextTwo(text: String)
+//    fun setButtonTextThree(text: String)
+//}
